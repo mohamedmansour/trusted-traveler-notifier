bae4333de453519528adb789922cfe71c67b8ec8
Mohamed Mansour <mmansour@microsoft.com>
2024-08-07 19:18:01 -0700

    feat: Add buttons view, logout, and login

--- extension/popup.js
+++ extension/popup.js
@@ -1,8 +1,13 @@
+const DASHBOARD_PAGE = 'https://ttp.cbp.dhs.gov/credential/v1/login?app=GOES-prod&lang=en&state=x4ig3T8VI3yOxECRd0tY2X';
+const LOGOUT_PAGE = 'https://secure.login.gov/openid_connect/logout?client_id=urn:gov:dhs:openidconnect.profiles:sp:sso:cbp:pspd-ttp-prod&post_logout_redirect_uri=https%3A%2F%2Fttp.cbp.dhs.gov%2Flogin%3Flogout%3Dlogout';
+const SCHEDULE_PAGE = 'https://ttp.cbp.dhs.gov/schedulerui/schedule-interview/location?lang=en&reqReason=true&token=&returnUrl=ttp-external-scheduler&service=nh&type=update&finExt=true';
+
 const port = chrome.runtime.connect({ name: 'popup' });
 
 function renderRefresh(message) {
   cookie.parentElement.title = `Last checked at: ${new Date().toLocaleString()}`;
   cookie.textContent = message === 'NotLoggedIn' ? 'Not logged in' : new Date(message).toLocaleString();
+  login.textContent = message === 'NotLoggedIn' ? 'Login' : 'Logout';
 }
 
 function renderSlot(message) {
@@ -17,6 +22,18 @@ function renderConnection(message) {
 
 connection.addEventListener('click', () => {
   port.postMessage({ action: 'toggle' });
+});
+
+login.addEventListener('click', () => {
+  if (login.textContent === 'Login') {
+    window.open(DASHBOARD_PAGE);
+  } else {
+    window.open(LOGOUT_PAGE);
+  }
+});
+
+view.addEventListener('click', () => {
+  window.open(SCHEDULE_PAGE)
 });
 
 port.onMessage.addListener((message) => {
